5905bacbfcc9c0b17f35ba8c7de8207cdee6f462
jsalvucci <jsalvucci@frvm.utn.edu.ar>
2021-08-07 16:39:23 -0300

    templatesVacios

--- Diseño/settings.py
+++ Diseño/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Diseño.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['VentraEntradas'],
+        'DIRS': ['Pantalla'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
